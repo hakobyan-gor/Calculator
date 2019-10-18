2cfb5359f24a796379c9e8c8f1b8c82697a40fef
Gor Hakobyan <gorhakobiann@gmail.com>
2019-10-18 16:44:53 +0400

    Some changes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 
 public class Controller {
 
@@ -11,8 +12,8 @@ public class Controller {
     private Label result;
 
     private StringBuilder output = new StringBuilder();
-    private Float number1 = null;
-    private Float number2 = null;
+    private String num1 = "";
+    private String num2 = "";
     private Character operator = null;
 
     public void processNumber(ActionEvent actionEvent) {
@@ -20,26 +21,29 @@ public class Controller {
         if (button.equals("0") && result.getText().equals("0")) {
             return;
         }
+        if (output.toString().length() > 12)
+            return;
         output.append(button);
         result.setText(output.toString());
+        resize(output.toString().length());
     }
 
     public void processOperator(ActionEvent actionEvent) {
         String button = ((Button) actionEvent.getSource()).getText();
         switch (button) {
             case "AC":
-                output.delete(0, output.length());
-                result.setText("0");
-                number1 = null;
-                number2 = null;
+                num1 = "";
+                num2 = "";
+                output = new StringBuilder();
                 operator = null;
+                resize(0);
+                result.setText("0");
                 break;
             case "±":
                 if (output.toString().isEmpty())
                     return;
                 if (output.charAt(0) == '-') {
-                    String temp = output.toString();
-                    output = new StringBuilder(temp.substring(1));
+                    output = new StringBuilder(output.substring(1));
                     result.setText(output.toString());
                     return;
                 }
@@ -55,97 +59,133 @@ public class Controller {
                     result.setText(output.toString());
                     return;
                 }
-
                 output.append(".");
                 result.setText(output.toString());
                 break;
             case "%":
-                if (number1 == null)
-                    number1 = Float.valueOf(output.toString());
-                number1 = calculate(number1);
-                operator = button.charAt(0);
-                output.delete(0, output.length());
-                result.setText(number1.toString());
+                if (num1.equals("")) {
+                    num1 = calculatePercent(output.toString());
+                    output = new StringBuilder();
+                    result.setText(num1);
+                    resize(num1.length());
+                } else if (operator != null) {
+                    output = new StringBuilder(calculatePercent(output.toString()));
+                    result.setText(output.toString());
+                    resize(output.length());
+                }
+                break;
             default:
-                process(button);
+                process(button.charAt(0));
                 break;
         }
 
     }
 
-    private void process(String button) {
+    private void process(Character operator) {
 
-        if (button.charAt(0) == '=') {
-            if (number2 == null) {
-                if (operator == '%')
-                    return;
-                number2 = Float.valueOf(output.toString());
+        String answer;
+
+        if (this.operator == null) {
+            num1 = output.toString();
+            this.operator = operator;
+            output = new StringBuilder();
+        } else if (!output.toString().isEmpty()) {
+            num2 = output.toString();
+            answer = calculate(num1, num2, this.operator);
+            num1 = answer;
+            num2 = "";
+            output = new StringBuilder();
+            if (operator != '=') {
+                this.operator = operator;
             }
-            if (operator != null && number1 != null) {
-                number1 = calculate(number1, number2, operator);
-                number2 = null;
-                operator = null;
-                output.delete(0, output.length());
-                if (number1.toString().substring(number1.toString().indexOf(".") + 1).equals("0")) {
-                    result.setText(number1.toString().substring(0, number1.toString().indexOf(".")));
-                    return;
-                }
-                result.setText(number1.toString());
+
+            if (answer == null) {
+                result.setText("Error");
+            } else if (answer.substring(answer.indexOf(".") + 1).equals("0")) {
+                result.setText(answer.substring(0, answer.indexOf(".")));
+                resize(answer.substring(0, answer.indexOf(".")).length());
+            } else {
+                result.setText(answer);
+                resize(answer.length());
             }
-            return;
-        }
-
-        if (number1 == null) {
-            number1 = Float.valueOf(output.toString());
-            output.delete(0, output.length());
-            operator = button.charAt(0);
-            return;
-        } else if (number2 == null && !output.toString().equals("")) {
-            number2 = Float.valueOf(output.toString());
-            output.delete(0, output.length());
-        } else {
-            operator = button.charAt(0);
-            return;
-        }
-
-        number1 = calculate(number1, number2, operator);
-        number2 = null;
-        operator = button.charAt(0);
-
-        if (number1 == null) {
-            output.delete(0, output.length());
-            result.setText("Error");
-        } else {
-            output.delete(0, output.length());
-            if (number1.toString().substring(number1.toString().indexOf(".") + 1).equals("0")) {
-                result.setText(number1.toString().substring(0, number1.toString().indexOf(".")));
-                return;
-            }
-            result.setText(number1.toString());
-        }
+        } else
+            this.operator = operator;
 
     }
 
-    private Float calculate(Float num1, Float num2, Character operator) {
+    private String calculate(String num1, String num2, Character operator) {
+
+        boolean n1 = num1.contains(".");
+        boolean n2 = num2.contains(".");
 
         switch (operator) {
             case '+':
-                return num1 + num2;
+                if (n1 && n2)
+                    return String.valueOf(Float.parseFloat(num1) + Float.parseFloat(num2));
+                if (!n1 && !n2)
+                    return String.valueOf(Long.parseLong(num1) + Long.parseLong(num2));
+                if (!n1)
+                    return String.valueOf(Long.parseLong(num1) + Float.parseFloat(num2));
+
+                return String.valueOf(Float.parseFloat(num1) + Long.parseLong(num2));
             case '×':
-                return num1 * num2;
+                if (n1 && n2)
+                    return String.valueOf(Float.parseFloat(num1) * Float.parseFloat(num2));
+                if (!n1 && !n2)
+                    return String.valueOf(Long.parseLong(num1) * Long.parseLong(num2));
+                if (!n1)
+                    return String.valueOf(Long.parseLong(num1) * Float.parseFloat(num2));
+
+                return String.valueOf(Float.parseFloat(num1) * Long.parseLong(num2));
             case '–':
-                return num1 - num2;
+                if (n1 && n2)
+                    return String.valueOf(Float.parseFloat(num1) - Float.parseFloat(num2));
+                if (!n1 && !n2)
+                    return String.valueOf(Long.parseLong(num1) - Long.parseLong(num2));
+                if (!n1)
+                    return String.valueOf(Long.parseLong(num1) - Float.parseFloat(num2));
+
+                return String.valueOf(Float.parseFloat(num1) - Long.parseLong(num2));
             case '÷':
-                if (num2 == 0.0 || num2 == 0)
+                if (num2.equals("0") || num2.equals("0.0"))
                     return null;
-                return num1 / num2;
+
+                if (n1 && n2)
+                    return String.valueOf(Float.parseFloat(num1) / Float.parseFloat(num2));
+                if (!n1 && !n2) {
+                    long $n1 = Long.parseLong(num1);
+                    long $n2 = Long.parseLong(num2);
+                    float temp = (float) $n1 / $n2;
+                    return String.valueOf(temp);
+                }
+                if (!n1)
+                    return String.valueOf(Long.parseLong(num1) / Float.parseFloat(num2));
+
+                return String.valueOf(Float.parseFloat(num1) / Long.parseLong(num2));
         }
 
         return null;
     }
 
-    private Float calculate(Float num1) {
+    private String calculatePercent(String num1) {
 
-        return num1 / 100;
+        if (num1.contains("."))
+            return String.valueOf(Float.parseFloat(num1) / 100);
+
+        int n = Integer.valueOf(num1);
+        float c = (float) n / 100;
+
+        return String.valueOf(c);
     }
+
+    private void resize(int lengthOfOutput) {
+        if (lengthOfOutput > 9) {
+            if (result.getFont().getSize() == 50)
+                result.setFont(Font.font(35));
+        } else {
+            if (result.getFont().getSize() == 35)
+                result.setFont(Font.font(50));
+        }
+    }
+
 }
